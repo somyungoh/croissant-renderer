cee429e265f120082432589270b98b0ec08d971d
David Oh <somyungoh@gmail.com>
2022-05-22 22:13:56 +0200

    Renderer: AA correction

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,7 +1,7 @@
 #include "GLFW/glfw3.h"
 
 #include "ray.h"
-#include "hittable.h"
+#include "hittable_list.h"
 #include "camera.h"
 
 #include <iostream>
@@ -11,7 +11,7 @@ const int render_h = 600;
 
 const int nSamples = 4;
 const int nSamplesW = glm::sqrt(nSamples);
-const float nSamplesOffset = 1.f / nSamples;
+const float nSamplesOffset = 0.5f / nSamplesW;
 
 float *pixmap = new float[render_w * render_h * 3];
 
@@ -23,27 +23,29 @@ void render()
     float           aspectRatio = (float)render_w / render_h;
     cr::CCamera     camera = cr::CCamera(glm::vec3(0, 0, -1), glm::vec3(0, 1, 0), glm::vec3(0, 0, 1), aspectRatio);
 
-    // Sphere
-    cr::CSphere     sphere = cr::CSphere(glm::vec3(0, 0, 0), 0.1);
+    // Scene
+    cr::CHittableList   world;
+    world.Add(std::make_shared<cr::CSphere>(cr::CSphere(glm::vec3(0, 0, 0), 0.1)));
+    world.Add(std::make_shared<cr::CSphere>(cr::CSphere(glm::vec3(0, -2, 0.5), 1.9)));
 
     for (size_t w = 0; w < render_w; w++) {
         for (size_t h = 0; h < render_h; h++) {
 #if 1
-            for (size_t s = 0; s < nSamples; s++) {
-                int si = s % nSamplesW;
-                int sj = s / nSamplesW;
-                float   u = (w + (float)si / nSamplesW + nSamplesOffset) / (render_w - 1);
-                float   v = (h + (float)sj / nSamplesW + nSamplesOffset) / (render_h - 1);
+            for (size_t s = 0; s < nSamples; s++)
+            {
+                int     si = s % nSamplesW;
+                int     sj = s / nSamplesW;
+                float   u = (w + (float)si / nSamplesW + nSamplesOffset) / render_w;
+                float   v = (h + (float)sj / nSamplesW + nSamplesOffset) / render_h;
                 cr::CRay    ray = camera.GetRay(u, v);
                 cr::SHitRec rec;
 
-                glm::vec3 color(u, v, (u + v) * 0.5f);
-                if (sphere.Hit(ray, rec))
-                {
-                    color.r = rec.n.r + 1.0;
-                    color.g = rec.n.g + 1.0;
-                    color.b = rec.n.b + 1.0;
-                }
+                glm::vec3 color;
+                if (world.Hit(ray, rec))
+                    color = rec.n + glm::vec3(1.0);
+                else
+                    color = glm::vec3(u, v, (u + v) * 0.5f);
+
                 pixmap[(h * render_w + w) * 3 + 0] += color.r / nSamples;
                 pixmap[(h * render_w + w) * 3 + 1] += color.g / nSamples;
                 pixmap[(h * render_w + w) * 3 + 2] += color.b / nSamples;
